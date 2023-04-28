c43bcf3b4577b10f20aac0d1b573e3aac13940e8
LUlisesRM <rmilanlu.cue@gmail.com>
2023-04-27 21:54:37 -0600

    Eliminacion de estilo Ventana modal

--- saco/Plataforma_citemsa/rrhh/base_de_datos/registrar.php
+++ saco/Plataforma_citemsa/rrhh/base_de_datos/registrar.php
@@ -17,7 +17,6 @@ $count = mysqli_num_rows($consulta);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../../estilo_saco.css">
-  <link rel="stylesheet" href="estilo.css">
   <title>SACO</title>
 
 </head>
@@ -79,72 +78,61 @@ $count = mysqli_num_rows($consulta);
             echo "<td>" . $row['vencimiento_licencia'] . "</td>";
             echo "</tr>";
           }
-
         } else {
           echo "<h1>Sin registro</h1>";
         }
 
         ?>
       </table>
-  
-      <div class="container-all" id="modal">
 
-        <div class="popup">
 
-          <div class="container-text">
 
-            <table border="2px">
-              <tr>
-                <th>REGISTRAR</th>
-              </tr>
-              <form action='insertar.php' method='post'>
-                <tr>
-                  <td>
-                    <lable>CREDENCIAL METROBUS:</lable>
-                    <input name='credencial_mb' type='text'>
-                    <lable>NOMINA:</lable>
-                    <input name='nomina' type='int'>
-                    <lable>CONTRASEÑA:</lable>
-                    <input name='contraseña' type='int'>
-                    <lable>APELLIDO PATERNO:</lable>
-                    <input name='apellido_paterno' type='text'> <br>
-                    <lable>APELLIDO MATERNO:</lable>
-                    <input name='apellido_materno' type='text'>
-                    <lable>NOMBRE:</lable>
-                    <input name='nombre' type='text'>
-                    <lable>ESTATUS:</lable>
-                    <input name='estatus' type='text'>
-                    <lable>FECHA DE CONTRATO:</lable>
-                    <input name='fecha_contrato' type='date'>
-                    <lable>TIPO DE LICENCIA:</lable>
-                    <input name='tipo_licencia' type='text'>
-                    <lable>ID LICENCIA:</lable>
-                    <input name='id_licencia' type='text'>
-                    <lable>VENCIMIENTO DE LICENCIA:</lable>
-                    <input name='vencimiento_licencia' type='date'>
-                    <lable>CURP:</lable>
-                    <input name='curp' type='text'>
-                    <lable>HOMOCLAVE:</lable>
-                    <input name='homoclave' type='text'>
-                    <lable>NUMERO DE SEGURO SOCIAL (NSS):</lable>
-                    <input name='nss' type='int'>
-                    <lable>TELEFONO:</lable>
-                    <input name='telefono' type='int'>
-                    <lable>CODIGO POSTAL:</lable>
-                    <input name='codigo_postal' type='int'>
-                    <br> <br>
-                    <input type='submit' value='ACEPTAR'> <button><a href='index.php'>CANCELAR</a></button>
-                  </td>
-                </tr>
-              </form>
-              <br>
-            </table>
-          </div>
-
-        </div>
-
-      </div>
-
+      <table border="2px">
+        <tr>
+          <th>REGISTRAR</th>
+        </tr>
+        <form action='insertar.php' method='post'>
+          <tr>
+            <td>
+              <lable>CREDENCIAL METROBUS:</lable>
+              <input name='credencial_mb' type='text'>
+              <lable>NOMINA:</lable>
+              <input name='nomina' type='int'> <br>
+              <lable>CONTRASEÑA:</lable>
+              <input name='contraseña' type='int'>
+              <lable>APELLIDO PATERNO:</lable>
+              <input name='apellido_paterno' type='text'> <br>
+              <lable>APELLIDO MATERNO:</lable>
+              <input name='apellido_materno' type='text'>
+              <lable>NOMBRE:</lable>
+              <input name='nombre' type='text'>
+              <lable>ESTATUS:</lable>
+              <input name='estatus' type='text'>
+              <lable>FECHA DE CONTRATO:</lable>
+              <input name='fecha_contrato' type='date'>
+              <lable>TIPO DE LICENCIA:</lable>
+              <input name='tipo_licencia' type='text'>
+              <lable>ID LICENCIA:</lable>
+              <input name='id_licencia' type='text'>
+              <lable>VENCIMIENTO DE LICENCIA:</lable>
+              <input name='vencimiento_licencia' type='date'>
+              <lable>CURP:</lable>
+              <input name='curp' type='text'>
+              <lable>HOMOCLAVE:</lable>
+              <input name='homoclave' type='text'>
+              <lable>NUMERO DE SEGURO SOCIAL (NSS):</lable>
+              <input name='nss' type='int'>
+              <lable>TELEFONO:</lable>
+              <input name='telefono' type='int'>
+              <lable>CODIGO POSTAL:</lable>
+              <input name='codigo_postal' type='int'>
+              <br> <br>
+              <input type='submit' value='ACEPTAR'> <button><a href='index.php'>CANCELAR</a></button>
+            </td>
+          </tr>
+        </form>
+        <br>
+      </table>
 
 
 
